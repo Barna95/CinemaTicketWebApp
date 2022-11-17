995ed4ce4eece25bb57c9ee3bfe5e30e9971ee04
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-11-17 18:02:29 +0100

    create : Controllers for models

---
+++ eTickets/eTickets/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Movies.Include(movie => movie.Cinema)
+                                            .OrderBy(m => m.Name)
+                                            .ToListAsync();
+            return View(data);
+        }
+    }
+}
