eadc4b8274d17e7e1d5dee7bb3f0b4eb83dab37e
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-11-17 23:26:53 +0100

    feat : Create cinema

--- eTickets/eTickets/Models/Cinema.cs
+++ eTickets/eTickets/Models/Cinema.cs
@@ -9,12 +9,15 @@ namespace eTickets.Models
         public int Id { get; set; }
 
         [Display(Name = "Cinema Logo")]
+        [Required(ErrorMessage = "Cinema Logo is required")]
         public string Logo { get; set; }
 
         [Display(Name = "Cinema Name")]
+        [Required(ErrorMessage = "Cinema name is required")]
         public string Name { get; set; }
 
         [Display(Name = "Description")]
+        [Required(ErrorMessage = "Cinema description is required")]
         public string Description { get; set; }
 
         //Relationships
