4f412067c42703422f0230a16a1074b3e760f6c5
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-11-17 14:12:34 +0100

    fix : AppDbContext config

--- eTickets/eTickets/Data/AppDbContext.cs
+++ eTickets/eTickets/Data/AppDbContext.cs
@@ -4,5 +4,9 @@ namespace eTickets.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+
+        }
     }
 }
