0e35d43723fbdfad602845bdbc99671147d3e9b7
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-11-17 19:22:16 +0100

    feat : Edit for Actors

--- eTickets/eTickets/Controllers/ActorsController.cs
+++ eTickets/eTickets/Controllers/ActorsController.cs
@@ -17,6 +17,8 @@ namespace eTickets.Controllers
             var data = await _service.GetAllAsync();
             return View(data);
         }
+
+        //Get: Actors/Create
         public IActionResult Create()
         {
             return View();
@@ -42,6 +44,25 @@ namespace eTickets.Controllers
                 return View("Not Found");
             }
             return View(actorDetails);
+        }
+
+        //Get: Actors/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,ProfilePictureURL,Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            await _service.UpdateAsync(id, actor);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
