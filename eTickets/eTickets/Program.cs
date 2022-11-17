abe861231ffd9cf1094aa6c80efa1b7be25931d7
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-11-17 14:43:01 +0100

    update : DbContext config

--- eTickets/eTickets/Program.cs
+++ eTickets/eTickets/Program.cs
@@ -1,4 +1,5 @@
 using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace eTickets
 {
@@ -9,7 +10,9 @@ namespace eTickets
             var builder = WebApplication.CreateBuilder(args);
 
             //DbContext config
-            builder.Services.AddDbContext<AppDbContext>();
+            builder.Services.AddDbContext<AppDbContext>(options =>options
+                            .UseSqlServer(builder.Configuration
+                            .GetConnectionString("DefaultConnectionString")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
