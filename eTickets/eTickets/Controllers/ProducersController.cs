950abe437edfe19c69cd3d1ba95f273412012076
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-11-17 21:48:46 +0100

    feat : Producer Details

--- eTickets/eTickets/Controllers/ProducersController.cs
+++ eTickets/eTickets/Controllers/ProducersController.cs
@@ -1,4 +1,5 @@
 ﻿using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,14 +7,14 @@ namespace eTickets.Controllers
 {
     public class ProducersController : Controller
     {
-        private readonly AppDbContext _context;
-        public ProducersController(AppDbContext context)
+        private readonly IProducersService _service;
+        public ProducersController(IProducersService service)
         {
-            _context = context;
+            _service = service;
         }
         public async Task<ActionResult> Index()
         {
-            var data = await _context.Producers.ToListAsync();
+            var data = await _service.GetAllAsync();
             return View(data);
         }
     }
