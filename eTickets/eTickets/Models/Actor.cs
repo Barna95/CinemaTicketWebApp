53b9854dcbce9348d81a92fb714f725764852336
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-11-17 14:20:38 +0100

    added : Model-db relationships

--- eTickets/eTickets/Models/Actor.cs
+++ eTickets/eTickets/Models/Actor.cs
@@ -10,5 +10,8 @@ namespace eTickets.Models
         public string ProfilePictureURL { get; set; }
         public string FullName { get; set; }
         public string Bio { get; set; }
+
+        // Relationships
+        public List<Actor_Movie> Actors_movies { get; set; }
     }
 }
--- eTickets/eTickets/Models/Movie.cs
+++ eTickets/eTickets/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using eTickets.Data.Enums;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eTickets.Models
 {
@@ -16,5 +17,18 @@ namespace eTickets.Models
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public MovieCategory MovieCategory { get; set; }
+
+        //Relationships
+        public List<Actor_Movie> Actors_Movies { get; set; }
+
+        //Cinema
+        public int CinemaId { get; set; }
+        [ForeignKey("CinemaId")]
+        public Cinema Cinema { get; set; }
+
+        //Cinema
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerId")]
+        public Producer Producer { get; set; }
     }
 }
