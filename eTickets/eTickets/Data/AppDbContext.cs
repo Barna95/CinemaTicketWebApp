425c4f44fc4981239577e053156ea7e76e197cc2
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-11-17 14:32:28 +0100

    feat : OnModelCreating method

--- eTickets/eTickets/Data/AppDbContext.cs
+++ eTickets/eTickets/Data/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
 
 namespace eTickets.Data
 {
@@ -6,7 +8,32 @@ namespace eTickets.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
         { 
-
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+            });
+
+            modelBuilder.Entity<Actor_Movie>()
+                .HasOne(m => m.Movie)
+                .WithMany(am => am.Actors_Movies)
+                .HasForeignKey(m => m.MovieId);
+
+            modelBuilder.Entity<Actor_Movie>()
+                .HasOne(m => m.Actor)
+                .WithMany(am => am.Actors_Movies)
+                .HasForeignKey(m => m.ActorId);
+
+            base.OnModelCreating(modelBuilder);
+
+            
+        }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Producer> Producers { get; set; }
     }
 }
