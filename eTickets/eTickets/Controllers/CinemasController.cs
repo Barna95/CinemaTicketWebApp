106e077e8ca2caad96d915a4f73c75b480c571c2
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-11-17 23:32:32 +0100

    feat : Details for cinemas

--- eTickets/eTickets/Controllers/CinemasController.cs
+++ eTickets/eTickets/Controllers/CinemasController.cs
@@ -37,5 +37,17 @@ namespace eTickets.Controllers
             await _service.AddAsync(cinema);
             return RedirectToAction(nameof(Index));
         }
+
+        //Get: Cinemas/Details/1
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if(cinemaDetails == null)
+            {
+                return View("NotFound");
+            }
+            return View(cinemaDetails);
+        }
     }
 }
