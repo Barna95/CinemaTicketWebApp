e0c6bd0ab81745ea8e375343571900b939a87ca0
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-11-17 22:52:50 +0100

    feat : Delete for producers

--- eTickets/eTickets/Controllers/ProducersController.cs
+++ eTickets/eTickets/Controllers/ProducersController.cs
@@ -77,5 +77,31 @@ namespace eTickets.Controllers
             }
             return View(producer);
         }
+
+        //GET: producers/delete/1
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null)
+            {
+                return View("NotFound");
+            }
+            return View(producerDetails);
+        }
+
+        [HttpPost,ActionName("DELETE")]
+
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null)
+            {
+                return View("NotFound");
+            }
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
