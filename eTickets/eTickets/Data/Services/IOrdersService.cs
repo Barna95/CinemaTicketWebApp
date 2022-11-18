79d244e496bc2e6f638d9b438ca26f54f19e46b7
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-11-18 06:11:49 +0100

    fix : Class changed to Interface

--- eTickets/eTickets/Data/Services/IOrdersService.cs
+++ eTickets/eTickets/Data/Services/IOrdersService.cs
@@ -2,7 +2,7 @@
 
 namespace eTickets.Data.Services
 {
-    public class IOrdersService
+    public interface IOrdersService
     {
         Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress);
         Task<List<Order>> GetOrdersByUserIdAsync(string userId);
