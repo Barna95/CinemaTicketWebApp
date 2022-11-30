a04cb82f0b542a087b8cc33f470427bfa347fa94
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-12-01 00:14:17 +0100

    add : Login-HttpPost

--- eTickets/eTickets/Controllers/AccountController.cs
+++ eTickets/eTickets/Controllers/AccountController.cs
@@ -20,5 +20,30 @@ namespace eTickets.Controllers
         }
 
         public IActionResult Login() => View(new LoginVM());
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
+                {
+                    var result = await _singInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Movies");
+                    }
+                }
+                TempData["Error"] = "Wrong credentials. Please, try again!";
+                return View(loginVM);
+            }
+
+            TempData["Error"] = "Wrong credentials. Please, try again!";
+            return View(loginVM);
+        }
     }
 }
