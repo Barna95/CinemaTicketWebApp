cd8b28ed7e44c11e34195d254daa815e2f8bcacb
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-11-30 23:28:36 +0100

    create : AccountController

---
+++ eTickets/eTickets/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using eTickets.Data;
+using eTickets.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
