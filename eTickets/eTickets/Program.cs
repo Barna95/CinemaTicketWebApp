c12be67f963f3490632a6eb1baff240e21a7c2fb
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-11-30 23:25:52 +0100

    add : Identity config

--- eTickets/eTickets/Program.cs
+++ eTickets/eTickets/Program.cs
@@ -1,6 +1,9 @@
 using eTickets.Data;
 using eTickets.Data.Cart;
 using eTickets.Data.Services;
+using eTickets.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -36,8 +39,14 @@ namespace eTickets
             builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             builder.Services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
 
+            // Authentication and authorization
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+            builder.Services.AddMemoryCache();
             builder.Services.AddSession();
-
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            });
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -60,13 +69,21 @@ namespace eTickets
             // Session
             app.UseSession();
 
+            // Authentication & Authorization
+
+            app.UseAuthentication();
+            app.UseAuthentication();
+
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
-
+            
+            // Seed database
             AppDbInitializer.Seed(app);
+            AppDbInitializer.SeedUsersAndRolesAsync(app).Wait();
+            
             app.Run();
         }
     }
