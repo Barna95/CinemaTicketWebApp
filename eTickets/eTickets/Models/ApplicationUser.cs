015e89f5a0f87013a9b59389a1f5cab44ab6b0ba
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-11-30 23:23:16 +0100

    create : User role model

---
+++ eTickets/eTickets/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace eTickets.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Display(Name = "Full name")]
+        public string FullName { get; set; }
+    }
+}
