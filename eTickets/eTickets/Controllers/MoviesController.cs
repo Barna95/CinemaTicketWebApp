3af13e98a066592ea61cb3e7a9a4da3b8cd46e6b
Erik <101416162+Eriknpy@users.noreply.github.com>
2023-03-15 16:27:59 +0100

    update : Searchbar query
    + Fix case sensitivity
    + Expanded search in descriptions

--- eTickets/eTickets/Controllers/MoviesController.cs
+++ eTickets/eTickets/Controllers/MoviesController.cs
@@ -33,11 +33,8 @@ namespace eTickets.Controllers
 
             if (!string.IsNullOrEmpty(searchString))
             {
-                //var filteredResult = allMovies.Where(n => n.Name.ToLower().Contains(searchString.ToLower()) || n.Description.ToLower().Contains(searchString.ToLower())).ToList();
-                
-
-                var filteredResultNew = allMovies.Where(n => string.Equals(n.Name, searchString, StringComparison.CurrentCultureIgnoreCase) || string.Equals(n.Description, searchString, StringComparison.CurrentCultureIgnoreCase)).ToList();
-                return View("Index", filteredResultNew);
+                var filteredResult = allMovies.Where(n => n.Name.ToLower().Contains(searchString.ToLower()) || n.Description.ToLower().Contains(searchString.ToLower())).ToList();
+                return View("Index", filteredResult);
             }
 
             return View("Index", allMovies);
